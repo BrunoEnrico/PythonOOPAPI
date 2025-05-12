f81d3999de1e2992bcc762c8463cfe04728c96cb
Bruno Fernandes <bruno.enrico99@gmail.com>
2025-05-12 14:55:06 -0300

    Studies regarding OOP

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
 from modelos.restaurante import Restaurante
+from modelos.cardapio.bebida import Bebida
+from modelos.cardapio.prato import Prato
 
 restaurante_praca = Restaurante('praça', 'Gourmet')
+bebida_suco = Bebida('Melancia', 3.50, tamanho="Grande")
+prato_salada = Prato('Salada', 15.99, descricao="Muito bom")
+
 restaurante_praca.receber_avaliacao('Gui', 10)
 restaurante_praca.receber_avaliacao('Lais', 8)
 restaurante_praca.receber_avaliacao('Emy', 2)
 
 def main():
-    Restaurante.listar_restaurantes()
+    print(bebida_suco)
+    print(prato_salada)
 
 if __name__ == '__main__':
     main()
--- modelos/cardapio/prato.py
+++ modelos/cardapio/prato.py
@@ -1,8 +1,13 @@
 from modelos.cardapio.item_cardapio import ItemCardapio
 
 class Prato(ItemCardapio):
-    def __init__(self, nome: str, preco: float, tamanho: str) -> None:
+    def __init__(self, nome: str, preco: float, descricao: str) -> None:
         super().__init__(nome=nome, preco=preco)
-        self._descricao = tamanho
+        self._nome = nome
+        self._preco = preco
+        self._descricao = descricao
+
+    def __str__(self):
+        return self._nome
 
         
