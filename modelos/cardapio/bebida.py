66975c7e053c4d7e7c82e7cfa3f65e4961474e57
Bruno Fernandes <bruno.fernandes.terceiros@claro.com.br>
2025-05-13 09:56:06 -0300

    Refactor to better use of OOP

--- modelos/cardapio/bebida.py
+++ modelos/cardapio/bebida.py
@@ -2,9 +2,7 @@ from modelos.cardapio.item_cardapio import ItemCardapio
 
 class Bebida(ItemCardapio):
     def __init__(self, nome: str, preco: float, tamanho: str) -> None:
-        super().__init__(nome=nome, preco=preco)
-        self._nome = nome
-        self._preco = preco
+        super().__init__(nome, preco)
         self._tamanho = tamanho
 
     def __str__(self):
