6077d3389568657b502cc2c31f7cde705da09808
Bruno Fernandes <bruno.fernandes.terceiros@claro.com.br>
2025-05-13 09:59:20 -0300

    method add to menu

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ from modelos.cardapio.prato import Prato
 restaurante_praca = Restaurante('praça', 'Gourmet')
 bebida_suco = Bebida('Melancia', 3.50, tamanho="Grande")
 prato_salada = Prato('Salada', 15.99, descricao="Muito bom")
+restaurante_praca.adicionar_ao_cardapio(bebida_suco)
+restaurante_praca.adicionar_ao_cardapio(prato_salada)
 
 restaurante_praca.receber_avaliacao('Gui', 10)
 restaurante_praca.receber_avaliacao('Lais', 8)
