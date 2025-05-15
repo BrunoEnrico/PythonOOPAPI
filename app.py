ce2db566ec642195f20332118c0320cf13673aaa
Bruno Fernandes <bruno.fernandes.terceiros@claro.com.br>
2025-05-15 11:49:40 -0300

    Added polymorphism to class with abstract methods

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ from modelos.cardapio.prato import Prato
 restaurante_praca = Restaurante('praça', 'Gourmet')
 bebida_suco = Bebida('Melancia', 3.50, tamanho="Grande")
 prato_salada = Prato('Salada', 15.99, descricao="Muito bom")
+bebida_suco.aplicar_desconto()
+prato_salada.aplicar_desconto()
 restaurante_praca.adicionar_ao_cardapio(bebida_suco)
 restaurante_praca.adicionar_ao_cardapio(prato_salada)
 
